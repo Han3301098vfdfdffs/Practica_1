ce9ac886785c27cbfdb0b780cf5120a3fb84ef2f
Hani <haniel.sor.3301@gmail.com>
2025-03-20 20:49:16 -0600

    three

--- app/src/main/java/com/example/practica_1/ui/practicaApp.kt
+++ app/src/main/java/com/example/practica_1/ui/practicaApp.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -39,77 +40,147 @@ import com.example.practica_1.data.DataSource
 import com.example.practica_1.model.Titulo
 import com.example.practica_1.viewmodel.MainViewModel
 
+//@Composable
+//fun ImagesCard(viewModel: MainViewModel,titulo: Titulo, isFirst: Boolean = false, modifier: Modifier = Modifier){
+//    var changeButton by remember{
+//        mutableStateOf(false)
+//    }
+//    LaunchedEffect(viewModel.countTime2) {
+//        if (viewModel.isCounterFinished) changeButton = false
+//    }
+//    Card(modifier = Modifier.padding(16.dp)){
+//        Row(verticalAlignment = Alignment.CenterVertically){
+//            Column(
+//                modifier = Modifier.weight(0.45f).padding(10.dp),
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            ) {
+//                Image(
+//                    painter = painterResource(id = titulo.drawableResourceId),
+//                    contentDescription = stringResource(id = titulo.stringResourceId),
+//                    modifier = Modifier
+//                        .size(150.dp)
+//                        .clip(CircleShape),
+//                    contentScale = ContentScale.Crop
+//                )
+//            }
+//            Column(
+//                modifier = Modifier.weight(0.6f).padding(10.dp)
+//            ){
+//                Text("${viewModel.countTime} [s]")
+//                Spacer(modifier = Modifier.height(30.dp))
+//                Text("${viewModel.countTime2} [s]")
+//                Spacer(modifier = Modifier.height(30.dp))
+//                Button(onClick = {
+//                        if(isFirst){
+//                            if(!changeButton) viewModel.goCounter() else viewModel.cancelarCounter()
+//                            changeButton = !changeButton
+//                        }
+//                        else{
+//                            if(!changeButton) viewModel.startCounter() else viewModel.cancelarCounter()
+//                            changeButton = !changeButton
+//                        }
+//                    },
+//                    modifier = Modifier.size(width = 150.dp, height = 50.dp),
+//                    colors = ButtonDefaults.buttonColors(
+//                        if(changeButton) {
+//                            Color.Red}  else Color.Green),
+//                ) {
+//                    Text(text = if(changeButton) "Cancelar" else "Iniciar")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun CardList(titulos: List<Titulo>, modifier: Modifier = Modifier){
+//    LazyColumn(modifier = modifier) {
+//        itemsIndexed(titulos){ index, titulo ->
+//            ImagesCard(
+//                viewModel = MainViewModel(),
+//                titulo = titulo,
+//                isFirst = index == 0,
+//                modifier= Modifier.padding(24.dp)
+//            )
+//        }
+//    }
+//}
+//
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun ImagesCard_Preview(){
+//    val dataSource = DataSource()
+//    val titulos = dataSource.LoadTitulos()
+//    ImagesCard(viewModel = MainViewModel(),titulo = titulos[0])
+//}
+
 @Composable
-fun ImagesCard(viewModel: MainViewModel,titulo: Titulo, isFirst: Boolean = false, modifier: Modifier = Modifier){
-    var changeButton by remember{
-        mutableStateOf(false)
-    }
-    LaunchedEffect(viewModel.countTime2) {
-        if (viewModel.isCounterFinished) changeButton = false
-    }
-    Card(modifier = Modifier.padding(16.dp)){
-        Row(verticalAlignment = Alignment.CenterVertically){
-            Column(
-                modifier = Modifier.weight(0.45f).padding(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(id = titulo.drawableResourceId),
-                    contentDescription = stringResource(id = titulo.stringResourceId),
-                    modifier = Modifier
-                        .size(150.dp)
-                        .clip(CircleShape),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Column(
-                modifier = Modifier.weight(0.6f).padding(10.dp)
-            ){
-                Text("${viewModel.countTime} [s]")
-                Spacer(modifier = Modifier.height(30.dp))
-                Text("${viewModel.countTime2} [s]")
-                Spacer(modifier = Modifier.height(30.dp))
-                Button(onClick = {
-                        if(isFirst){
-                            if(!changeButton) viewModel.goCounter() else viewModel.cancelarCounter()
-                            changeButton = !changeButton
-                        }
-                        else{
-                            if(!changeButton) viewModel.startCounter() else viewModel.cancelarCounter()
-                            changeButton = !changeButton
-                        }
-                    },
-                    modifier = Modifier.size(width = 150.dp, height = 50.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        if(changeButton) {
-                            Color.Red}  else Color.Green),
-                ) {
-                    Text(text = if(changeButton) "Cancelar" else "Iniciar")
-                }
-            }
+fun TestAlgorithmScreen(
+    viewModel: MainViewModel,
+    n: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "Ejecución secuencial y concurrente:",
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Button(
+            onClick = { viewModel.calcularSuma(1) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Sumar")
         }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { viewModel.calcularSuma(2) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Sumar 2N")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { viewModel.calcularSuma(3) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Sumar 3N")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { viewModel.calcularSuma(4) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Sumar 4N")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { viewModel.calcularSuma(5) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Sumar 5N")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { viewModel.resetResultados() },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Borrar resultados")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Resultados:",
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(text = "Suma lineal: ${viewModel.sumaLineal}")
+        Text(text = "Suma acumulativa: ${viewModel.sumaAcumulativa}")
+        Text(text = "Veces sumadas: ${viewModel.vecesSumadas}")
     }
 }
 
+@Preview
 @Composable
-fun CardList(titulos: List<Titulo>, modifier: Modifier = Modifier){
-    LazyColumn(modifier = modifier) {
-        itemsIndexed(titulos){ index, titulo ->
-            ImagesCard(
-                viewModel = MainViewModel(),
-                titulo = titulo,
-                isFirst = index == 0,
-                modifier= Modifier.padding(24.dp)
-            )
-        }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun ImagesCard_Preview(){
-    val dataSource = DataSource()
-    val titulos = dataSource.LoadTitulos()
-    ImagesCard(viewModel = MainViewModel(),titulo = titulos[0])
+fun TestAlgorithmScreenPreview() {
+    val viewModel = MainViewModel()
+    TestAlgorithmScreen(viewModel = viewModel, n = 1)
 }
--- app/src/main/java/com/example/practica_1/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/practica_1/viewmodel/MainViewModel.kt
@@ -73,4 +73,24 @@ class MainViewModel: ViewModel() {
         countTime = 0
         countTime2 = 0
     }
+    //SUMA
+
+    var sumaLineal by mutableStateOf(0)
+        private set
+    var sumaAcumulativa by mutableStateOf(0)
+        private set
+    var vecesSumadas by mutableStateOf(0)
+        private set
+
+
+    fun calcularSuma(multiplier: Int) {
+        sumaLineal = 1 + 2  // Siempre 3, sin importar el botón presionado.
+        sumaAcumulativa += 5 * multiplier
+        vecesSumadas += multiplier
+    }
+    fun resetResultados() {
+        sumaLineal = 0
+        sumaAcumulativa = 0
+        vecesSumadas = 0
+    }
 }
