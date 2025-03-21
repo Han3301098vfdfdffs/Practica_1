4e4e81fbd05e237fe0d5f9fd4b9d9afe66c5ad56
Hani <haniel.sor.3301@gmail.com>
2025-03-20 19:29:45 -0600

    Two

--- app/src/main/java/com/example/practica_1/MainActivity.kt
+++ app/src/main/java/com/example/practica_1/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.practica_1.ui.practicaApp
 import com.example.practica_1.ui.theme.Practica_1Theme
 import com.example.practica_1.data.DataSource
 import com.example.practica_1.ui.CardList
@@ -30,10 +29,6 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ImageCard(
                         modifier =Modifier.padding(innerPadding)
-                    )
-                    practicaApp(
-                        viewModel,
-                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
